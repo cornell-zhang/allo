420b2f2444cce9778f9c4620bf3d0d803d915189
chhzh123 <hc676@cornell.edu>
2023-07-27 13:10:06 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@
 import setuptools
 
 
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
 def setup():
     with open("README.md", encoding="utf-8") as fp:
         long_description = fp.read()
@@ -16,10 +22,7 @@ def setup():
         long_description=long_description,
         long_description_content_type="text/markdown",
         setup_requires=[],
-        install_requires=[
-            "packaging",
-            "psutil",
-        ],
+        install_requires=parse_requirements("requirements.txt"),
         packages=setuptools.find_packages(),
         url="https://github.com/cornell-zhang/allo",
         python_requires=">=3.8",
