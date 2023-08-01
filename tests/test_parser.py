fb698b5aa2cc82f1205230343d4c544009c4dbb5
chhzh123 <hc676@cornell.edu>
2023-08-01 15:07:11 -0400

    [primitive] Support partition output & compose

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -736,6 +736,42 @@ def test_no_init():
     print(s.module)
 
 
+def test_output_partition_compose():
+    M, N, K = 4, 4, 4
+
+    def Linear_layer(
+        inp: float32[M, K], W: float32[K, N], B: float32[N]
+    ) -> float32[M, N]:
+        outp: float32[M, N] = 0.0
+        for i, j in allo.grid(M, N, name="gemm"):
+            for k in allo.reduction(K):
+                outp[i, j] += inp[i, k] * W[k, j]
+        for i, j in allo.grid(M, N, name="bias"):
+            outp[i, j] += B[j]
+        return outp
+
+    def Add(inp1: float32[M, N], inp2: float32[M, N]) -> float32[M, N]:
+        outp: float32[M, N] = 0.0
+        for i, j in allo.grid(M, N, name="add"):
+            outp[i, j] = inp1[i, j] + inp2[i, j]
+        return outp
+
+    def Top(inp: float32[M, K], W: float32[K, N], B: float32[N]) -> float32[M, N]:
+        add1 = Linear_layer(inp, W, B)
+        add2 = Linear_layer(inp, W, B)
+        outp1 = Add(add1, add2)
+        return outp1
+
+    s_ll = allo.customize(Linear_layer)
+    s_ll.partition(Linear_layer.outp, partition_type=1, dim=2, factor=2)
+    s = allo.customize(Top)
+    s.compose(s_ll)
+    print(s.module)
+
+    f = s.build(target="vhls")
+    print(f)
+
+
 if __name__ == "__main__":
     test_gemm_grid_for()
     test_gemm_range_for()
@@ -764,3 +800,4 @@ if __name__ == "__main__":
     test_compose_nested()
     test_no_init()
     test_double_partition()
+    test_output_partition_compose()
