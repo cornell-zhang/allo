75bef96520ca995b7d0bf3ea99e7b5abc589d892
Mengjia Dai <md2249@brg-zhang-xcel.ece.cornell.edu>
2023-08-08 06:41:19 -0400

    commit create_pr

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -6,49 +6,42 @@ from allo.ir.types import int32
 import numpy as np
 
 
-def same(A: int32[32, 1]) -> int32[32, 1]:
-    return A
+def test_same():
+    def same(A: int32[32, 32]) -> int32[32, 32]:
+        return A
+
+    s = allo.customize(same, enable_tensor=True)
+    print(s.module)
 
 
-def outzero() -> int32:
-    C: int32 = 0
-    return C
+def test_same_scalar():
+    def same_scalar(A: int32) -> int32:
+        return A
+
+    s = allo.customize(same_scalar, enable_tensor=True)
+    print(s.module)
 
 
-# test_td_outzero = """
-# func.func @outzero() -> tensor<i32> {
-#   %0 = tensor.generate  {
-#     %c0_i32 = arith.constant 0 : i32
-#     tensor.yield %c0_i32 :i32
-#   } : tensor<i32>
-#   return %0 : tensor<i32>
-# }
-# """
-# mod = allo.invoke_mlir_parser(test_td_outzero)
-# print(mod)
+def test_outzero():
+    def outzero() -> int32[32, 32]:
+        C: int32[32, 32] = 0
+        return C
+
+    s = allo.customize(outzero, enable_tensor=True)
+    print(s.module)
 
 
-def half(A: int32[2, 2]) -> int32[1, 1]:
-    return A[:1, :1]
+def test_outzero_scalar():
+    def outzero_scalar() -> int32:
+        C: int32 = 0
+        return C
+
+    s = allo.customize(outzero_scalar, enable_tensor=True)
+    print(s.module)
 
 
-s = allo.customize(outzero, verbose=True, enable_tensor=True)
-print(s.module)
-
-
-# test_td_half = """
-# func.func @half(%A: tensor<32x32xi32>) -> tensor<16x16xi32> {
-#    %01 = tensor.extract_slice %A[0, 0][16, 16][1, 1] : tensor<32x32xi32> to tensor<16x16xi32>
-#    return %0 : tensor<16x16xi32>
-#  }
-# """
-# mod = allo.invoke_mlir_parser(test_td_half)
-# print(mod)
-
-# test_td_same_1 = """
-# func.func @same(%arg0: tensor<i32>) -> tensor<i32> {
-#     return %arg0 : tensor<i32>
-#   }
-# """
-# mod = allo.invoke_mlir_parser(test_td_same_1)
-# print(mod)
+if __name__ == "__main__":
+    test_same()
+    test_same_scalar()
+    test_outzero()
+    test_outzero_scalar()
