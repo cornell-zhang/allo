4bdf6a7348410e015243616d94dc3bec4284538a
Mengjia Dai <md2249@brg-zhang-xcel.ece.cornell.edu>
2023-08-16 15:33:14 -0400

    create_pr

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -18,6 +18,7 @@ def test_same():
     np.testing.assert_allclose(np_A, np_A_allo, rtol=1e-5)
 
 
+#  scalars are not transformed into tensors even if enable_tensor=True
 def test_same_scalar():
     def same_scalar(A: int32) -> int32:
         return A
@@ -26,7 +27,7 @@ def test_same_scalar():
     print(s.module)
 
     mod = s.build()
-    np_A = np.zeros(1, dtype=np.int32)
+    np_A = 0
     np_A_allo = mod(np_A)
     np.testing.assert_allclose(np_A, np_A_allo, rtol=1e-5)
 
@@ -45,6 +46,7 @@ def test_outzero():
     np.testing.assert_allclose(np_C, np_C_allo, rtol=1e-5)
 
 
+#  scalars are not transformed into tensors even if enable_tensor=True
 def test_outzero_scalar():
     def outzero_scalar() -> int32:
         C: int32 = 0
@@ -54,7 +56,7 @@ def test_outzero_scalar():
     print(s.module)
 
     mod = s.build()
-    np_C = np.zeros(1, dtype=np.int32)
+    np_C = 0
     np_C_allo = mod()
     np.testing.assert_allclose(np_C, np_C_allo, rtol=1e-5)
 
