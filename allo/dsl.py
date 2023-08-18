e4e7a2ee56131cdcc736d262ba92bd5bdb1ec825
chhzh123 <hc676@cornell.edu>
2023-08-18 11:28:42 -0400

    Fix dsl name

--- allo/dsl.py
+++ allo/dsl.py
@@ -5,78 +5,78 @@
 import numpy as np
 
 
-def grid(*args, name=""):
+def grid(*args, name=None):
     return np.ndindex(*args)
 
 
-def reduction(*args, name=""):
+def reduction(*args, name=None):
     return np.ndindex(*args)
 
 
-def matmul(lhs, rhs):
+def matmul(lhs, rhs, name=None):
     return np.matmul(lhs, rhs)
 
 
-def bmm(lhs, rhs):
+def bmm(lhs, rhs, name=None):
     return np.einsum("ijk,ikn->ijn", lhs, rhs)
 
 
-def add(lhs, rhs):
+def add(lhs, rhs, name=None):
     return lhs + rhs
 
 
-def sub(lhs, rhs):
+def sub(lhs, rhs, name=None):
     return lhs - rhs
 
 
-def div(lhs, rhs):
+def div(lhs, rhs, name=None):
     return lhs / rhs
 
 
-def exp(x):
+def exp(x, name=None):
     return np.exp(x)
 
 
-def log(x):
+def log(x, name=None):
     return np.log(x)
 
 
-def log2(x):
+def log2(x, name=None):
     return np.log2(x)
 
 
-def log10(x):
+def log10(x, name=None):
     return np.log10(x)
 
 
-def abs(x):
+def abs(x, name=None):
     return np.abs(x)
 
 
-def softmax(x):
+def softmax(x, name=None):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0)
 
 
-def sqrt(x):
+def sqrt(x, name=None):
     return np.sqrt(x)
 
 
-def sin(x):
+def sin(x, name=None):
     return np.sin(x)
 
 
-def cos(x):
+def cos(x, name=None):
     return np.cos(x)
 
 
-def tan(x):
+def tan(x, name=None):
     return np.tan(x)
 
 
-def tanh(x):
+def tanh(x, name=None):
     return np.tanh(x)
 
 
-def power(x, y):
+def power(x, y, name=None):
     return np.power(x, y)
