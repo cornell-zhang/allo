2f41a301cc20d42bff835e726317bdb61c3a44f0
Zhichen Zeng <108505471+Zhichenzzz@users.noreply.github.com>
2023-08-28 23:11:38 -0400

    [Frontend] Add PyTorch frontend call_module builder with torch.fx (#62)
    * [Frontend] Add PyTorch frontend builder with torch.fx
    
    * add new file mlp.py and fix problems

---
+++ examples/mlp.py
@@ -0,0 +1,33 @@
+# Copyright Allo authors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import allo
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear1 = torch.nn.Linear(30, 30)
+        self.linear2 = torch.nn.Linear(30, 30)
+
+    def forward(self, data):
+        out = self.linear1(data)
+        out = self.linear2(out)
+        out = F.relu(out)
+        return out
+
+
+model = MLP()
+model.eval()
+example_inputs = [torch.rand(30, 30)]
+llvm_mod = allo.frontend.from_pytorch(
+    model, example_inputs=example_inputs, verbose=True
+)
+
+golden = model(*example_inputs)
+np_inputs = [x.detach().numpy() for x in example_inputs]
+res = llvm_mod(*np_inputs)
+torch.testing.assert_close(res, golden.detach().numpy())
