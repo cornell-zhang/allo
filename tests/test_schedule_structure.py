fe6ea075dde7e18341b534a3f6cfdd6f4502a0ed
chhzh123 <hc676@cornell.edu>
2023-08-30 09:45:59 -0400

    Add function test

--- tests/test_schedule_structure.py
+++ tests/test_schedule_structure.py
@@ -4,7 +4,7 @@
 import numpy as np
 import pytest
 import allo
-from allo.ir.types import int32
+from allo.ir.types import index, int32
 
 
 @pytest.mark.parametrize("axes", [[0], [1], [0, 1]])
@@ -22,6 +22,31 @@ def test_systolic_array(axes):
     s.unfold("PE", axes=axes)
     assert str(s.module).count('from = "A"') == len(axes) * M
     assert str(s.module).count('to = "B"') == len(axes) * M
+    print(s.module)
+    np_A = np.random.randint(0, 10, size=(M, N))
+    np_B = np_A + 1
+    mod = s.build()
+    res_allo = mod(np_A)
+    np.testing.assert_allclose(res_allo, np_B)
+
+
+@pytest.mark.parametrize("axes", [[0], [1], [0, 1]])
+def test_nested_function(axes):
+    M, N = 2, 2
+
+    def kernel(A: int32[M, N], B: int32[M, N], i: index, j: index):
+        B[i, j] = A[i, j] + 1
+
+    def matrix_add(A: int32[M, N]) -> int32[M, N]:
+        B: int32[M, N]
+        for i, j in allo.grid(M, N, name="PE"):
+            kernel(A, B, i, j)
+        return B
+
+    s = allo.customize(matrix_add)
+    print(s.module)
+    s.unfold("PE", axes=axes)
+    assert str(s.module).count("func.call @kernel") == len(axes) * M
     print(s.module)
     np_A = np.random.randint(0, 10, size=(M, N))
     np_B = np_A + 1
