2f9a4e75062073e352c25d55881498180eafaae5
chhzh123 <hc676@cornell.edu>
2023-09-10 14:21:13 -0400

    Move np helper functions to utils

--- allo/backend/llvm.py
+++ allo/backend/llvm.py
@@ -28,6 +28,14 @@ from ..utils import (
     get_bitwidth_from_type,
     get_bitwidth_and_frac_from_fixed,
     get_clostest_pow2,
+    ctype_map,
+    np_supported_types,
+    np_type_to_str,
+    is_anywidth_int_type_and_not_np,
+    handle_overflow,
+    make_anywidth_numpy_array,
+    struct_array_to_int_array,
+    get_np_struct_type,
 )
 
 
@@ -47,222 +55,6 @@ def ranked_memref_to_numpy(ranked_memref):
         np.ctypeslib.as_array(ranked_memref[0].strides) * np_arr.itemsize,
     )
     return to_numpy(strided_arr)
-
-
-np_supported_types = {
-    "f32": np.float32,
-    "f64": np.float64,
-    "i8": np.int8,
-    "i16": np.int16,
-    "i32": np.int32,
-    "i64": np.int64,
-    "ui8": np.uint8,
-    "ui16": np.uint16,
-    "ui32": np.uint32,
-    "ui64": np.uint64,
-}
-
-
-ctype_map = {
-    "f32": ctypes.c_float,
-    "f64": ctypes.c_double,
-    "i8": ctypes.c_int8,
-    "i16": ctypes.c_int16,
-    "i32": ctypes.c_int32,
-    "i64": ctypes.c_int64,
-    "ui8": ctypes.c_uint8,
-    "ui16": ctypes.c_uint16,
-    "ui32": ctypes.c_uint32,
-    "ui64": ctypes.c_uint64,
-}
-
-
-def np_type_to_str(dtype):
-    return list(np_supported_types.keys())[
-        list(np_supported_types.values()).index(dtype)
-    ]
-
-
-def get_np_pow2_type(bitwidth, signed=True):
-    if bitwidth <= 8:
-        return np.int8 if signed else np.uint8
-    if bitwidth <= 16:
-        return np.int16 if signed else np.uint16
-    if bitwidth <= 32:
-        return np.int32 if signed else np.uint32
-    if bitwidth <= 64:
-        return np.int64 if signed else np.uint64
-    raise RuntimeError("Unsupported bitwidth")
-
-
-def get_np_struct_type(bitwidth):
-    n_bytes = int(np.ceil(bitwidth / 8))
-    return np.dtype(
-        {
-            "names": [f"f{i}" for i in range(n_bytes)],
-            # all set to unsigned byte
-            "formats": ["u1"] * n_bytes,
-            "offsets": list(range(n_bytes)),
-            "itemsize": n_bytes,
-        }
-    )
-
-
-def is_anywidth_int_type_and_not_np(dtype):
-    return str(dtype) not in np_supported_types and (
-        str(dtype).startswith("i") or str(dtype).startswith("ui")
-    )
-
-
-def make_anywidth_numpy_array(array, bitwidth):
-    """
-    Converts a numpy array to any target bitwidth.
-    ----------------
-    Parameters:
-    array: numpy.ndarray
-        numpy array, can be any numpy native bitwidth, e.g. np.int64
-    bitwidth: int
-        target bitwidth e.g. 9, 31, 198
-    ----------------
-    Returns:
-    numpy.ndarray
-        numpy array with the target bitwidth
-    """
-    shape = array.shape
-    sign_array = array >= 0
-    avail_bytes = array.itemsize  # number of bytes of each element
-    # The following code has several steps to convert the numpy array to have
-    # the correct data type in order to create an MLIR constant tensor.
-    # Since MLIR-NumPy Python interface only supports byte-addressable data types,
-    # we need to change the data type of the array to have the minimum number of bytes
-    # that can represent the target bitwidth.
-    # e.g., hcl.const_tensor(arr, dtype=hcl.Int(20)) (6*6 array)
-    #       which requires 20 bits (3 bytes) to represent each element
-    # declaration: 6*6*i20
-    # numpy input: 6*6*i64
-    # 1. Decompose the original i32 or i64 array into a structured array of uint8
-    #  -> decompose: 6*6*8*i8
-    # pylint: disable=no-else-return
-    if bitwidth == 1:
-        return np.packbits(array, axis=None, bitorder="little")
-    else:
-        # Here we construct a customized NumPy dtype, "f0", "f1", "f2", etc.
-        # are the field names, and the entire data type is `op.values.dtype`.
-        # This can be viewed as a `union` type in C/C++.
-        # Please refer to the documentation for more details:
-        # https://numpy.org/doc/stable/reference/arrays.dtypes.html#specifying-and-constructing-data-types
-        decomposed_np_dtype = np.dtype(
-            (
-                array.dtype,
-                {f"f{i}": (np.uint8, i) for i in range(array.dtype.itemsize)},
-            )
-        )
-        array = array.view(decomposed_np_dtype)
-        # 2. Compose the uint8 array into a structured array of target bitwidth
-        # This is done by taking the first several bytes of the uint8 array
-        # "u1" means one unsigned byte, and "i1" means one signed byte
-        # f0 is LSB, fn is MSB
-        # [[(f0, f1, ..., f7), (f0, f1, ..., f7)], ...]
-        n_bytes = int(np.ceil(bitwidth / 8))
-        new_dtype = get_np_struct_type(bitwidth)
-        # Take each byte as a separate array
-        # [f0  [f1  [f2
-        #  ..   ..   ..
-        #  f0]  f1]  f2]
-        _bytes = [array[f"f{i}"] for i in range(min(avail_bytes, n_bytes))]
-        # sometimes the available bytes are not enough to represent the target bitwidth
-        # so that we need to pad the array
-        if avail_bytes < n_bytes:
-            padding = np.where(sign_array, 0x00, 0xFF).astype(np.uint8)
-            _bytes += [padding] * (n_bytes - avail_bytes)
-        # Stack them together
-        # -> compose: 6*6*3*i8
-        array = np.stack(_bytes, axis=-1)
-        # -> flatten: 108*i8
-        array = array.flatten()
-        # -> view: 36*i24
-        array = array.view(np.dtype(new_dtype))
-        # -> reshape: 6*6*i24
-        array = array.reshape(shape)
-        return array
-
-
-def struct_array_to_int_array(array, bitwidth, signed=True):
-    """
-    Converts a structured numpy array to back to an integer array.
-    ----------------
-    Parameters:
-    array: numpy.ndarray
-        A structured numpy array
-    bitwidth: int
-        target bitwidth e.g. 9, 31, 198
-    signed: bool
-        whether the target type is signed or not
-    ----------------
-    Returns:
-    numpy.ndarray
-        numpy array in np.int64
-    """
-    # TODO: Sometimes we need to use Python native list to do the conversion
-    #       since Numpy array cannot hold very long-bitwidth integer (>64)
-    #       See: https://github.com/cornell-zhang/heterocl/pull/493
-    # e.g., numpy input 6*6*i24
-    # 1. Take each byte as a separate array
-    # [f0  [f1  [f2
-    #  ..   ..   ..
-    #  f0]  f1]  f2]
-    # -> unflatten: 6*6*3*i8
-    shape = array.shape
-    n_bytes = int(np.ceil(bitwidth / 8))
-    if bitwidth > 64:
-        raise RuntimeError("Cannot convert data with bitwidth > 64 to numpy array")
-    target_bytes = max(get_clostest_pow2(bitwidth), 8) // 8
-    if n_bytes == 1:
-        _bytes = [array] if array.dtype == np.uint8 else [array["f0"]]
-    else:
-        _bytes = [array[f"f{i}"] for i in range(n_bytes)]
-    # Take the negative sign part
-    # Find the MSB
-    bit_idx = (bitwidth - 1) % 8
-    sign_array = (_bytes[-1] & (1 << bit_idx)) > 0
-    # Need to also set _bytes[-1]
-    if signed:
-        _bytes[-1][sign_array] |= (0xFF << bit_idx) & 0xFF
-    for _ in range(len(_bytes), target_bytes):
-        if signed:
-            sign = np.zeros_like(_bytes[0], dtype=np.uint8)
-            sign[sign_array] = 0xFF
-            _bytes.append(sign)
-        else:
-            _bytes.append(np.zeros_like(_bytes[0], dtype=np.uint8))
-    # Stack them together
-    # compose: 6*6*4*i8
-    array = np.stack(_bytes, axis=-1)
-    # -> flatten: 144*i8
-    array = array.flatten()
-    # -> view: 36*i32
-    array = array.view(get_np_pow2_type(bitwidth))
-    # -> reshape: 6*6*i32
-    array = array.reshape(shape)
-    return array
-
-
-def handle_overflow(np_array, bitwidth, dtype):
-    if dtype.startswith("fixed") or dtype.startswith("ufixed"):
-        # Round to nearest integer towards zero
-        np_dtype = np.int64 if dtype.startswith("fixed") else np.uint64
-        np_array = np.fix(np_array).astype(np_dtype)
-    sb = 1 << bitwidth
-    sb_limit = 1 << (bitwidth - 1)
-    np_array = np_array % sb
-
-    if dtype.startswith("fixed") or dtype.startswith("i"):
-
-        def cast_func(x):
-            return x if x < sb_limit else x - sb
-
-        return np.vectorize(cast_func)(np_array)
-    return np_array
 
 
 def invoke_mlir_parser(mod: str):
@@ -385,8 +177,8 @@ class LLVMModule:
                         f"Input type mismatch: {np_type} vs {target_in_type}"
                     ).warn()
                 if is_anywidth_int_type_and_not_np(target_in_type):
-                    width = get_bitwidth_from_type(target_in_type)
-                    arg = handle_overflow(arg, width, target_in_type)
+                    bitwidth = get_bitwidth_from_type(target_in_type)
+                    arg = handle_overflow(arg, bitwidth, target_in_type)
                     # This is to be compliant with MLIR's anywidth int type alignment
                     # e.g. i1-i8 -> int8
                     #      i9-i16 -> int16
@@ -394,7 +186,6 @@ class LLVMModule:
                     #      i33-i64 -> int64
                     #      i65-i128 -> int128
                     #      i129-i256 -> int256
-                    bitwidth = max(get_clostest_pow2(width), 8)
                     # pylint: disable=redefined-variable-type
                     arg = make_anywidth_numpy_array(arg, bitwidth)
                 elif target_in_type in np_supported_types:
@@ -409,7 +200,6 @@ class LLVMModule:
                     bitwidth, frac = get_bitwidth_and_frac_from_fixed(target_in_type)
                     arg = arg * (2**frac)
                     arg = handle_overflow(arg, bitwidth, target_in_type)
-                    bitwidth = max(get_clostest_pow2(bitwidth), 8)
                     arg = make_anywidth_numpy_array(arg, bitwidth)
                 else:
                     raise RuntimeError(
