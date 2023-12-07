97f18b2ef0dd5574b8498aa28af758a5fe8da4a2
Niansong Zhang <nz264@cornell.edu>
2023-12-07 13:30:05 -0500

    Add functional testing to durbin

--- examples/polybench/durbin.py
+++ examples/polybench/durbin.py
@@ -1,22 +1,36 @@
 # Copyright Allo authors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+import os
+import json
+import pytest
 import allo
 import numpy as np
-from allo.ir.types import float32, int32
+from allo.ir.types import int32, float32
+import allo.ir.types as T
 
 
-def top_durbin(size="tiny"):
-    if size == "mini":
-        N = 40
-    elif size == "small":
-        N = 120
-    elif size == "medium":
-        N = 400
-    elif size is None or size == "tiny":
-        N = 4
+def durbin_np(r, y):
+    z = np.zeros_like(y)
+    N = r.shape[0]
+    y[0] = -r[0]
+    beta = 1.0
+    alpha = -r[0]
+    for k in range(1, N):
+        beta = (1 - alpha * alpha) * beta
+        sum_ = 0.0
+        for i in range(k):
+            sum_ = sum_ + r[k - i - 1] * y[i]
+        alpha = -1.0 * (r[k] + sum_) / beta
+        for i in range(k):
+            z[i] = y[i] + alpha * y[k - i - 1]
+        for i in range(k):
+            y[i] = z[i]
+        y[k] = alpha
 
-    def kernel_durbin(r: float32[N], y: float32[N]):
+
+def durbin(concrete_type, n):
+    def kernel_durbin[T: (float32, int32), N: int32](r: "T[N]", y: "T[N]"):
         y[0] = -r[0]
         beta: float32 = 1.0
         alpha: float32 = -r[0]
@@ -39,17 +53,27 @@ def top_durbin(size="tiny"):
 
             y[k] = alpha
 
-    s0 = allo.customize(kernel_durbin)
-    orig = s0.build("vhls")
+    s = allo.customize(kernel_durbin, instantiate=[concrete_type, n])
+    return s.build()
 
-    s = allo.customize(kernel_durbin)
-    opt = s.build("vhls")
 
-    return orig, opt
+def test_durbin():
+    # read problem size settings
+    setting_path = os.path.join(os.path.dirname(__file__), "psize.json")
+    with open(setting_path, "r") as fp:
+        psize = json.load(fp)
+    # for CI test we use small problem size
+    test_psize = "small"
+    N = psize["durbin"][test_psize]["N"]
+    concrete_type = float32
+    r = np.random.randint(-10, 10, size=(N,)).astype(np.float32)
+    y = np.random.randint(-10, 10, size=(N,)).astype(np.float32)
+    y_golden = y.copy()
+    durbin_np(r, y_golden)
+    mod = durbin(concrete_type, N)
+    mod(r.copy(), y)
+    np.testing.assert_allclose(y, y_golden, rtol=1e-3, atol=1e-3)
 
 
 if __name__ == "__main__":
-    orig, opt = top_durbin()
-    from cedar.verify import verify_pair
-
-    verify_pair(orig, opt, "durbin", liveout_vars="v1")
+    pytest.main([__file__])
