1ed8541ccadcabcb76c86a01f89ce971311ee633
chhzh123 <hc676@cornell.edu>
2024-01-26 15:07:38 -0500

    Add pytest

--- tests/pytorch/test_linear.py
+++ tests/pytorch/test_linear.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import pytest
 import torch
 from torch import nn
 import allo
@@ -35,41 +35,44 @@ class TestModule(nn.Module):
         return self.fc(hidden_states)
 
 
-model = TestModule(D, bias=False)
-model.eval()
-x = torch.randint(-10, 10, (L, D), device=device, dtype=torch.float32)
-with torch.no_grad():
-    out = model(x)
+def test_int8_linear():
+    model = TestModule(D, bias=False)
+    model.eval()
+    x = torch.randint(-10, 10, (L, D), device=device, dtype=torch.float32)
+    with torch.no_grad():
+        out = model(x)
+
+    def linear(X: int8[L, D], W: int8[D, 4 * D]) -> int8[L, 4 * D]:
+        Z: int8[L, 4 * D]
+        systolic[int8, int8, int8, L, D, 4 * D, 2, 2](X, W, Z)
+        return Z
+
+    s_linear = allo.customize(linear)
+    mod = s_linear.build()
+    np_x = x.numpy().astype(np.int8)
+    # important to have "ascontiguousarray"
+    np_w = model.fc.weight.detach().T.numpy().astype(np.int8)
+    np_w = np.ascontiguousarray(np_w)
+    allo_out = mod(np_x, np_w)
+    np.testing.assert_allclose(allo_out, np_x @ np_w, atol=1e-3)
+    print("Passed Numpy test!")
+    np.testing.assert_allclose(allo_out, out.numpy().astype(np.int8), atol=1e-3)
+    print("Passed PyTorch test!")
+
+    s_linear.compose(systolic, instantiate=[int8, int8, int8, L, D, 4 * D, 2, 2])
+    hls_mod = s_linear.build(
+        target="vitis_hls",
+        mode="csim",
+        project=f"test_linear.prj",
+    )
+    csim_out = np.zeros((L, 4 * D), dtype=np.int8)
+    if os.system(f"which vitis_hls >> /dev/null") != 0:
+        print("Vitis HLS not found, skipping...")
+        return
+    hls_mod(np_x, np_w, csim_out)
+    np.testing.assert_allclose(csim_out, allo_out, atol=1e-3)
+    print("Passed HLS csim test!")
 
 
-def linear(X: int8[L, D], W: int8[D, 4 * D]) -> int8[L, 4 * D]:
-    Z: int8[L, 4 * D]
-    systolic[int8, int8, int8, L, D, 4 * D, 2, 2](X, W, Z)
-    return Z
-
-
-s_linear = allo.customize(linear)
-mod = s_linear.build()
-np_x = x.numpy().astype(np.int8)
-# important to have "ascontiguousarray"
-np_w = model.fc.weight.detach().T.numpy().astype(np.int8)
-np_w = np.ascontiguousarray(np_w)
-allo_out = mod(np_x, np_w)
-np.testing.assert_allclose(allo_out, np_x @ np_w, atol=1e-3)
-print("Passed Numpy test!")
-np.testing.assert_allclose(allo_out, out.numpy().astype(np.int8), atol=1e-3)
-print("Passed PyTorch test!")
-
-s_linear.compose(systolic, instantiate=[int8, int8, int8, L, D, 4 * D, 2, 2])
-hls_mod = s_linear.build(
-    target="vitis_hls",
-    mode="csim",
-    project=f"test_linear.prj",
-)
-csim_out = np.zeros((L, 4 * D), dtype=np.int8)
-if os.system(f"which vitis_hls >> /dev/null") != 0:
-    print("Vitis HLS not found, skipping...")
-    sys.exit()
-hls_mod(np_x, np_w, csim_out)
-np.testing.assert_allclose(csim_out, allo_out, atol=1e-3)
-print("Passed HLS csim test!")
+if __name__ == "__main__":
+    pytest.main([__file__])
