477de003f31484d30a9ad967f4bb5f027e6c3a56
chhzh123 <hc676@cornell.edu>
2024-09-19 01:14:08 -0400

    Fix decorator

--- allo/dataflow.py
+++ allo/dataflow.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # pylint: disable=no-name-in-module, unexpected-keyword-arg, no-value-for-parameter
 
+import functools
 from types import FunctionType as PyFunctionType
 from hcl_mlir.ir import (
     StringAttr,
@@ -22,9 +23,14 @@ def move_stream_to_interface(func):
     stream_ops = []
     stream_types = []
     for op in func.entry_block.operations:
-        if isinstance(op, memref_d.AllocOp) and "stream" in MemRefType(op.result.type).memory_space.value:
+        if (
+            isinstance(op, memref_d.AllocOp)
+            and "stream" in MemRefType(op.result.type).memory_space.value
+        ):
             stream_ops.append(op)
             stream_types.append(MemRefType(op.result.type))
+    if len(stream_ops) == 0:
+        return func
     in_types = func.attributes["function_type"].value.inputs
     out_types = func.attributes["function_type"].value.results
     in_types += stream_types
@@ -35,11 +41,15 @@ def move_stream_to_interface(func):
         ip=InsertionPoint(func),
     )
     func_op.add_entry_block()
+    func_op.attributes["itypes"] = func.attributes["itypes"]
+    func_op.attributes["otypes"] = func.attributes["otypes"]
     # copy function operations
     cnt_stream = 0
     for op in func.entry_block.operations:
         if op in stream_ops:
-            op.result.replace_all_uses_with(func_op.arguments[len(func_op.entry_block.arguments) - 1 + cnt_stream])
+            op.result.replace_all_uses_with(
+                func_op.arguments[len(func_op.entry_block.arguments) - 1 + cnt_stream]
+            )
             cnt_stream += 1
             continue
         op.operation.clone(InsertionPoint(func_op.entry_block))
@@ -54,57 +64,60 @@ def kernel(mapping=None):
         # Just for locating insertion point
         pass
 
-    def decorator(func):
-        # construct a common module
-        s_top = customize(top)
-        global_vars = _get_global_vars(func)
-        new_global_vars = global_vars.copy()
-        for var in global_vars.values():
-            # import functions from other files
-            if isinstance(var, PyFunctionType):
-                new_global_vars.update(_get_global_vars(var))
-        # call different PE kernels
-        with s_top.module.context, Location.unknown():
-            assert len(mapping) <= 2, "Only support 1D/2D mapping now."
-            for dim in np.ndindex(*mapping):
-                global_vars = new_global_vars.copy()
-                if len(dim) == 1:
-                    global_vars.update({"df.pi": dim[0]})
-                    new_func_name = func.__name__ + f"_{dim[0]}"
-                else:
-                    global_vars.update({"df.pi": dim[0], "df.pj": dim[1]})
-                    new_func_name = func.__name__ + f"_{dim[0]}_{dim[1]}"
-                s = customize(
-                    func, global_vars=global_vars, context=s_top.module.context
+    def actual_decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # *args and **kwargs are the actual arguments that are passed into the kernel function
+            # construct a common module
+            s_top = customize(top)
+            global_vars = _get_global_vars(func)
+            new_global_vars = global_vars.copy()
+            for var in global_vars.values():
+                # import functions from other files
+                if isinstance(var, PyFunctionType):
+                    new_global_vars.update(_get_global_vars(var))
+            # call different PE kernels
+            with s_top.module.context, Location.unknown():
+                assert len(mapping) <= 2, "Only support 1D/2D mapping now."
+                for dim in np.ndindex(*mapping):
+                    global_vars = new_global_vars.copy()
+                    if len(dim) == 1:
+                        global_vars.update({"df.pi": dim[0]})
+                        new_func_name = func.__name__ + f"_{dim[0]}"
+                    else:
+                        global_vars.update({"df.pi": dim[0], "df.pj": dim[1]})
+                        new_func_name = func.__name__ + f"_{dim[0]}_{dim[1]}"
+                    s = customize(
+                        func, global_vars=global_vars, context=s_top.module.context
+                    )
+                    s.top_func = move_stream_to_interface(s.top_func)
+                    s.top_func.attributes["sym_name"] = StringAttr.get(new_func_name)
+                    s.top_func.operation.clone(InsertionPoint(s_top.top_func))
+                top_func = func_d.FuncOp(
+                    name="top", type=s.top_func.type, ip=InsertionPoint(s_top.top_func)
                 )
-                s.top_func = move_stream_to_interface(s.top_func)
-                print(s.module)
-                sys.exit()
-                s.top_func.attributes["sym_name"] = StringAttr.get(new_func_name)
-                s.top_func.operation.clone(InsertionPoint(s_top.top_func))
-            top_func = func_d.FuncOp(
-                name="top", type=s.top_func.type, ip=InsertionPoint(s_top.top_func)
-            )
-            top_func.add_entry_block()
-            top_func.attributes["itypes"] = s.top_func.attributes["itypes"]
-            top_func.attributes["otypes"] = s.top_func.attributes["otypes"]
-            func_d.ReturnOp([], ip=InsertionPoint(top_func.entry_block))
-            for dim in np.ndindex(*mapping):
-                new_func_name = func.__name__ + f"_{'_'.join(map(str, dim))}"
-                func_d.CallOp(
-                    [],
-                    FlatSymbolRefAttr.get(new_func_name),
-                    top_func.arguments,
-                    ip=InsertionPoint.at_block_terminator(top_func.entry_block),
-                )
-            top_func.attributes["dataflow"] = UnitAttr.get()
-        s_top.top_func.operation.erase()
-        s_top.top_func = top_func
-        print(s_top.module)
-        exe = s_top.build()
-        return exe
+                top_func.add_entry_block()
+                top_func.attributes["itypes"] = s.top_func.attributes["itypes"]
+                top_func.attributes["otypes"] = s.top_func.attributes["otypes"]
+                func_d.ReturnOp([], ip=InsertionPoint(top_func.entry_block))
+                for dim in np.ndindex(*mapping):
+                    new_func_name = func.__name__ + f"_{'_'.join(map(str, dim))}"
+                    func_d.CallOp(
+                        [],
+                        FlatSymbolRefAttr.get(new_func_name),
+                        top_func.arguments,
+                        ip=InsertionPoint.at_block_terminator(top_func.entry_block),
+                    )
+                top_func.attributes["dataflow"] = UnitAttr.get()
+            s_top.top_func.operation.erase()
+            s_top.top_func = top_func
+            print(s_top.module)
+            exe = s_top.build()
+            return exe(*args, **kwargs)
 
-    return decorator
+        return wrapper
+
+    return actual_decorator
 
 
 def get_pid():
@@ -113,3 +126,18 @@ def get_pid():
 
 def pipe():
     raise NotImplementedError("This function should be called in a kernel function.")
+
+
+def build(funcs):
+    def top():
+        # Just for locating insertion point
+        pass
+
+    # construct a common module
+    s_top = customize(top)
+    with s_top.module.context, Location.unknown():
+        for func in funcs:
+            s = customize(func.__wrapped__, context=s_top.module.context)
+            s.top_func = move_stream_to_interface(s.top_func)
+            s.top_func.operation.clone(InsertionPoint(s_top.top_func))
+    print(s_top.module)
