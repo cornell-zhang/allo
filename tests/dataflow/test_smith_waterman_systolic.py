84a0bae61b1abe60f660ed8d450aac3ff67ddce6
chhzh123 <hc676@cornell.edu>
2024-11-08 11:57:42 -0500

    Fix smith-waterman

--- tests/dataflow/test_smith_waterman_systolic.py
+++ tests/dataflow/test_smith_waterman_systolic.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import allo
-from allo.ir.types import int8, int32, Stream
+from allo.ir.types import int8, int32
 import allo.dataflow as df
 import allo.backend.hls as hls
 import numpy as np
@@ -11,7 +11,7 @@ import numpy as np
 W: int32 = 2  # gap penalty score per length, assuming a linear increament
 
 max_size = 100
-M, N = np.random.randint(30, max_size), np.random.randint(30, max_size)
+M, N = 4, 4
 P0, P1 = M + 1, N + 1
 
 Sim: int32 = 3
@@ -36,41 +36,41 @@ def smith_waterman_score_matrix(seqA, seqB):
     return score_matrix
 
 
-@df.kernel(mapping=[P0, P1])
-def Smith_Waterman(A: int8[M], B: int8[N], S: int32[P0, P1]):
-    i, j = df.get_pid()
-    in_A: Stream[int32] = df.pipe(src=(i, j - 1), dst=(i, j))
-    in_B: Stream[int32] = df.pipe(src=(i - 1, j), dst=(i, j))
-    in_C: Stream[int32] = df.pipe(src=(i - 1, j - 1), dst=(i, j))
-    out_B: Stream[int32] = df.pipe(src=(i, j), dst=(i + 1, j))
-    out_A: Stream[int32] = df.pipe(src=(i, j), dst=(i, j + 1))
-    out_C: Stream[int32] = df.pipe(src=(i, j), dst=(i + 1, j + 1))
+@df.region()
+def top():
+    fifo_A = df.array(df.pipe(dtype=int32, shape=(), depth=4), shape=(P0, P1))
+    fifo_B = df.array(df.pipe(dtype=int32, shape=(), depth=4), shape=(P0, P1))
+    fifo_C = df.array(df.pipe(dtype=int32, shape=(), depth=4), shape=(P0, P1))
 
-    with allo.meta_if(i == 0 and j == 0):
-        # Fill the first row and column with zeros
-        out_A.put(0)
-        out_B.put(0)
-        out_C.put(0)
-    with allo.meta_elif(i == 0):
-        out_A.put(0)
-        out_C.put(0)
-    with allo.meta_elif(j == 0):
-        out_B.put(0)
-        out_C.put(0)
-    with allo.meta_else():
-        a = in_A.get()
-        b = in_B.get()
-        c = in_C.get()
-        # Calculate the score for the current position
-        aligning: int32 = c + (Sim if A[i - 1] == B[j - 1] else Mis)
-        gap_A: int32 = a - W
-        gap_B: int32 = b - W
-        # Choose the maximum score
-        score: int32 = max(max(0, aligning), max(gap_A, gap_B))
-        S[i, j] = score
-        out_A.put(max(gap_A, score))
-        out_B.put(max(gap_B, score))
-        out_C.put(score)
+    @df.kernel(mapping=[P0, P1])
+    def Smith_Waterman(A: int8[M], B: int8[N], S: int32[P0, P1]):
+        i, j = df.get_pid()
+        with allo.meta_if(i == 0 and j == 0):
+            # Fill the first row and column with zeros
+            fifo_A[i, j].put(0)
+            fifo_B[i, j].put(0)
+            fifo_C[i, j].put(0)
+        with allo.meta_elif(i == 0):
+            fifo_A[i, j].put(0)
+            fifo_C[i, j].put(0)
+        with allo.meta_elif(j == 0):
+            fifo_B[i, j].put(0)
+            fifo_C[i, j].put(0)
+        with allo.meta_else():
+            a = fifo_A[i, j].get()
+            b = fifo_B[i, j].get()
+            c = fifo_C[i, j].get()
+            # Calculate the score for the current position
+            aligning: int32 = c + (Sim if A[i - 1] == B[j - 1] else Mis)
+            gap_A: int32 = a - W
+            gap_B: int32 = b - W
+            # Choose the maximum score
+            score: int32 = max(max(0, aligning), max(gap_A, gap_B))
+            S[i, j] = score
+            fifo_A[i, j + 1].put(max(gap_A, score))
+            fifo_B[i + 1, j].put(max(gap_B, score))
+            fifo_C[i + 1, j + 1].put(score)
+        # TODO: Add draining modules
 
 
 def test_systolic():
@@ -78,14 +78,13 @@ def test_systolic():
     A = np.random.choice(possible_chars, size=M).view(np.int8)
     B = np.random.choice(possible_chars, size=N).view(np.int8)
     S = np.zeros((P0, P1), dtype=np.int32)
-    print("awaiting hls...")
+    mod = df.build(top)
     if hls.is_available("vitis_hls"):
-        Smith_Waterman(A, B, S)
+        mod(A, B, S)
         oracle = smith_waterman_score_matrix(A, B)
         np.testing.assert_array_equal(S, oracle)
         print("Passed!")
 
 
 if __name__ == "__main__":
-    for _ in range(10):
-        test_systolic()
+    test_systolic()
