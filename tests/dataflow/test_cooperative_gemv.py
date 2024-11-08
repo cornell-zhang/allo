efd29cd9f565b2b202f734be4e76ce564ae9a2cd
chhzh123 <hc676@cornell.edu>
2024-11-07 22:01:11 -0500

    Fix cooperative

--- tests/dataflow/test_cooperative_gemv.py
+++ tests/dataflow/test_cooperative_gemv.py
@@ -1,7 +1,7 @@
 # Copyright Allo authors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from allo.ir.types import float32, Stream
+from allo.ir.types import float32
 import allo.dataflow as df
 import allo.backend.hls as hls
 import numpy as np
@@ -12,32 +12,33 @@ P0 = 2
 Mt = M // P0
 
 
-@df.kernel(mapping=[P0])
-def gemv0(A: Ty[M, N], x: Ty[N]):
-    pi = df.get_pid()
-    pipe: Stream[Ty[Mt]] = df.pipe(src="gemv0", dst="gemv1")
-    y_out: Ty[Mt] = 0
-    for m in range(pi * Mt, (pi + 1) * Mt):
-        y_acc: Ty = 0
-        for n in range(N // 2):
-            y_acc += A[m, n] * x[n]
-        y_out[m - pi * Mt] = y_acc
-    pipe.put(y_out)
+@df.region()
+def top():
+    pipe = df.pipe(dtype=Ty, shape=(Mt,), depth=2)
 
+    @df.kernel(mapping=[P0])
+    def gemv0(A: Ty[M, N], x: Ty[N]):
+        pi = df.get_pid()
+        y_out: Ty[Mt] = 0
+        for m in range(pi * Mt, (pi + 1) * Mt):
+            y_acc: Ty = 0
+            for n in range(N // 2):
+                y_acc += A[m, n] * x[n]
+            y_out[m - pi * Mt] = y_acc
+        pipe.put(y_out)
 
-@df.kernel(mapping=[P0])
-def gemv1(A: Ty[M, N], x: Ty[N], y: Ty[M]):
-    pi = df.get_pid()
-    pipe: Stream[Ty[Mt]] = df.pipe(src="gemv0", dst="gemv1")
-    y_out: Ty[Mt] = 0
-    for m in range(pi * Mt, (pi + 1) * Mt):
-        y_acc: Ty = 0
-        for n in range(N // 2, N):
-            y_acc += A[m, n] * x[n]
-        y_out[m - pi * Mt] = y_acc
-    y_prev: Ty[Mt] = pipe.get()
-    for m in range(pi * Mt, (pi + 1) * Mt):
-        y[m] = y_out[m - pi * Mt] + y_prev[m - pi * Mt]
+    @df.kernel(mapping=[P0])
+    def gemv1(A: Ty[M, N], x: Ty[N], y: Ty[M]):
+        pi = df.get_pid()
+        y_out: Ty[Mt] = 0
+        for m in range(pi * Mt, (pi + 1) * Mt):
+            y_acc: Ty = 0
+            for n in range(N // 2, N):
+                y_acc += A[m, n] * x[n]
+            y_out[m - pi * Mt] = y_acc
+        y_prev: Ty[Mt] = pipe.get()
+        for m in range(pi * Mt, (pi + 1) * Mt):
+            y[m] = y_out[m - pi * Mt] + y_prev[m - pi * Mt]
 
 
 def test_cooperative_gemv():
@@ -46,9 +47,9 @@ def test_cooperative_gemv():
         N,
     ).astype(np.float32)
     C = np.zeros((M,), dtype=np.float32)
-    top = df.build([gemv0, gemv1])
+    mod = df.build(top)
     if hls.is_available("vitis_hls"):
-        top(A, B, C)
+        mod(A, B, C)
         np.testing.assert_allclose(C, np.dot(A, B), atol=1e-5)
         print("Passed!")
 
