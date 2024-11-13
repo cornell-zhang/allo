765444f7c7a05fe5b90866bd7acea22c5f63a25d
adrianliu <adrianliu00@gmail.com>
2024-11-13 16:34:05 -0500

    update unified systolic

--- tests/dataflow/test_unified_systolic.py
+++ tests/dataflow/test_unified_systolic.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import allo
-from allo.ir.types import int32, bool, Stream
+from allo.ir.types import int32, bool
 import allo.dataflow as df
 import allo.backend.hls as hls
 import numpy as np
