f7c6363cc12cd5644452cf57ec6b6f430ec22fa7
Jiahan Xie <jx353@cornell.edu>
2024-11-24 22:46:20 -0500

    pass 2 by 2 case

--- tests/dataflow/test_packed_systolic.py
+++ tests/dataflow/test_packed_systolic.py
@@ -32,66 +32,58 @@ def top():
     ):
         i, j = df.get_pid()
         # Peripheral kernels
-        with allo.meta_if(i in {0, M + 1} and j in {0, N + 1}):
+        with allo.meta_if(i in {0, M // PP + 1} and j in {0, N + 1}):
             pass
         with allo.meta_elif(j == 0):
             # i > 0
-            for k in range(K // PP):
+            for k in range(K):
                 fifo_A[i, j + 1].put(X_packed[i - 1, k])
         with allo.meta_elif(i == 0):
             # j > 0
-            for k in range(K // PP):
-                fifo_B[i + 1, j].put(W_packed[k, (j - 1)])
+            for k in range(K):
+                fifo_B[i + 1, j].put(W_packed[j // PP, 0])
+
         # drain
-        with allo.meta_elif(i == M + 1 and j > 0):
-            for k in range(K // PP):
+        with allo.meta_elif(i == M // PP + 1 and j > 0):
+            for k in range(K):
                 b: allo_type = fifo_B[i, j].get()
         with allo.meta_elif(j == N + 1 and i > 0):
-            for k in range(K // PP):
+            for k in range(K):
                 a: allo_type = fifo_A[i, j].get()
         # main body
         with allo.meta_else():
-            c: allo_type = 0
+            Z_elm: allo_type = Z_packed[i - 1, j - 1]
             for k in range(K):
+                c: allo_type = 0
                 a: allo_type = fifo_A[i, j].get()
                 b: allo_type = fifo_B[i, j].get()
                 for p in range(PP):
                     a_unpacked: int8 = a[p * 8 : (p + 1) * 8]
                     b_unpacked: int8 = b[p * 8 : (p + 1) * 8]
-                    c[p * 8 : (p + 1) * 8] += a_unpacked * b_unpacked
+                    c += a_unpacked * b_unpacked
                 fifo_A[i, j + 1].put(a)
                 fifo_B[i + 1, j].put(b)
-                Z_packed[i - 1, j - 1][k * 8 : (k + 1) * 8] += c
+                Z_elm[k * 8 : (k + 1) * 8] += c
+            Z_packed[i - 1, j - 1] = Z_elm
 
 
 def test_packed_systolic():
     X = np.random.randint(-4, 4, size=(L, D)).astype(np.int8)
-    print("X:")
-    print(X)
     W_A_cst = np.random.randint(-4, 4, size=(D, 1 * D)).astype(np.int8)
-    print("W_A_cst:")
-    print(W_A_cst)
 
     packed_X = np.ascontiguousarray(np.ascontiguousarray(X).view(np_type).transpose())
-    print("packed_X:")
-    print(packed_X)
     W_A_packed = np.ascontiguousarray(
         np.ascontiguousarray(W_A_cst.transpose()).view(np_type).transpose()
     )
-    print("W_A_packed")
-    print(W_A_packed)
     Z_packed = np.zeros((L // PP, 1 * D), dtype=np_type)
     mod = df.build(top)
     if hls.is_available("vitis_hls"):
         mod(packed_X, W_A_packed, Z_packed)
+
         np_C = X @ W_A_cst
-        print("np_C:")
-        print(np_C)
         np_C_packed = np.ascontiguousarray(
-            np.ascontiguousarray(np_C).view(np_type).transpose()
+            np.ascontiguousarray(np_C.transpose()).view(np_type).transpose()
         )
-        print("np_C_packed:")
-        print(np_C_packed)
         np.testing.assert_allclose(Z_packed, np_C_packed, atol=1e-3)
         print("Passed!")
 
