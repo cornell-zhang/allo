605779b606b892cdbc6c7a68d4dae0169cda3d93
chhzh123 <hc676@cornell.edu>
2025-03-06 02:21:40 -0500

    Fix header

--- allo/backend/aie_kernels/add.cc
+++ allo/backend/aie_kernels/add.cc
@@ -1,3 +1,7 @@
+/*
+ * Copyright Allo authors. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 //===- scale.cc -------------------------------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
