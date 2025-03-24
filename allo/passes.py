77b9494b07506cc007243c6f7d8b7092b9797826
chhzh123 <hc676@cornell.edu>
2025-03-23 20:43:56 -0400

    Format

--- allo/passes.py
+++ allo/passes.py
@@ -785,7 +785,7 @@ def analyze_read_write_patterns(mlir_func):
     }
 
     # Walk through all operations in the function to analyze access patterns
-    for arg_index in range(len(mlir_func.arguments)):
+    for arg_index, _ in enumerate(mlir_func.arguments):
         if not isinstance(mlir_func.arguments[arg_index].type, MemRefType):
             continue
         is_output = False
