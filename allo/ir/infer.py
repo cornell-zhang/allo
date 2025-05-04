1bfe8d2e067e1cc1c1fda40054b60688259dbbc9
Jiahan Xie <jx353@cornell.edu>
2025-05-04 10:09:01 -0400

    logic bug

--- allo/ir/infer.py
+++ allo/ir/infer.py
@@ -953,8 +953,9 @@ class TypeInferer(ASTVisitor):
                     argAshape[2] - argBshape[2] + 1,
                     argAshape[3] - argBshape[3] + 1,
                 )
-            elif op_name in {"maxpool"}:
-                argBshape = ASTResolver.resolve_constant(new_args[1], ctx)
+            elif op_name in {"maxpool", "sumpool"}:
+                if op_name == "maxpool":
+                    argBshape = ASTResolver.resolve_constant(new_args[1], ctx)
                 node.shape = (
                     argAshape[0],
                     argAshape[1],
