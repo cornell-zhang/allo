9afa858879ab520a656a54ca8f3fa2a4f7f2b814
Huajie Zhong <hz.ansonzhong@gmail.com>
2025-05-04 17:43:08 -0400

    format

--- allo/frontend/pytorch.py
+++ allo/frontend/pytorch.py
@@ -145,7 +145,7 @@ class TorchBuilder:
             for name, buf in self.named_buffers.items():
                 new_name = name.replace(".", "_")
                 if buf.shape:
-                    shape_str = ', '.join([str(s) for s in buf.shape])
+                    shape_str = ", ".join([str(s) for s in buf.shape])
                     res += f"    {new_name}: float32[{shape_str}] = gb_{new_name}\n"
                 else:
                     res += f"    {new_name}: float32 = gb_{new_name}\n"
@@ -220,7 +220,7 @@ class TorchBuilder:
             F.softmax: "softmax",
             F.linear: "linear",
             F.gelu: "gelu",
-            F.relu: "relu", 
+            F.relu: "relu",
             F.dropout: "identity",
             torch.tril: "tril",
             torch.cat: "concat",
@@ -484,19 +484,38 @@ class TorchBuilder:
             name_id = self.get_unique_id("conv2d")
 
             self.composition.append(
-                ("conv2d", name_id, [float32, B, Cin, Cout, H, W, Kh, Kw, Oh, Ow, stride[0], stride[1], padding[0], padding[1]],
+                (
+                    "conv2d",
+                    name_id,
+                    [
+                        float32,
+                        B,
+                        Cin,
+                        Cout,
+                        H,
+                        W,
+                        Kh,
+                        Kw,
+                        Oh,
+                        Ow,
+                        stride[0],
+                        stride[1],
+                        padding[0],
+                        padding[1],
+                    ],
                 )
             )
-            if dilation != (1,1):
-                raise NotImplementedError(f"Unsupported conv2d with dilation: {dilation}")
+            if dilation != (1, 1):
+                raise NotImplementedError(
+                    f"Unsupported conv2d with dilation: {dilation}"
+                )
 
             if has_bias:
                 return f'{node.name} = nn.conv2d[float32, {B}, {Cin}, {Cout}, {H}, {W}, {Kh}, {Kw}, {Oh}, {Ow}, {stride[0]}, {stride[1]}, {padding[0]}, {padding[1]}, "{name_id}"]({inp}, {weight}, {bias})'
-            else:
-                raise NotImplementedError(f"Unsupported conv2d without bias")
+            raise NotImplementedError(f"Unsupported conv2d without bias")
         else:
             raise NotImplementedError(f"Unsupported shape for conv: {input_shape}")
-    
+
     def build_maxpool2d(self, node):
         module = self.get_module(node.target)
         inp = get_var_name(node.args[0])
@@ -515,14 +534,17 @@ class TorchBuilder:
             name_id = self.get_unique_id("maxpool2d")
 
             self.composition.append(
-                ("maxpool2d", name_id, [float32, B, C, H, W, K, Oh, Ow, stride, padding],
+                (
+                    "maxpool2d",
+                    name_id,
+                    [float32, B, C, H, W, K, Oh, Ow, stride, padding],
                 )
             )
-            
+
             return f'{node.name} = nn.maxpool2d[float32, {B}, {C}, {H}, {W}, {K}, {Oh}, {Ow}, {stride}, {padding}, "{name_id}"]({inp})'
         else:
             raise NotImplementedError(f"Unsupported shape for maxpool2d: {input_shape}")
-    
+
     def build_avgpool2d(self, node):
         module = self.get_module(node.target)
         inp = get_var_name(node.args[0])
@@ -541,14 +563,17 @@ class TorchBuilder:
             name_id = self.get_unique_id("avgpool2d")
 
             self.composition.append(
-                ("avgpool2d", name_id, [float32, B, C, H, W, K, Oh, Ow, stride, padding],
+                (
+                    "avgpool2d",
+                    name_id,
+                    [float32, B, C, H, W, K, Oh, Ow, stride, padding],
                 )
             )
-            
+
             return f'{node.name} = nn.avgpool2d[float32, {B}, {C}, {H}, {W}, {K}, {Oh}, {Ow}, {stride}, {padding}, "{name_id}"]({inp})'
         else:
             raise NotImplementedError(f"Unsupported shape for avgpool2d: {input_shape}")
-    
+
     def build_batchnorm2d(self, node):
         module = self.get_module(node.target)
         inp = get_var_name(node.args[0])
@@ -567,10 +592,10 @@ class TorchBuilder:
 
             name_id = self.get_unique_id("batchnorm2d")
 
-            self.composition.append(
-                ("batchnorm2d", name_id, [float32, B, C, H, W])
-            )
+            self.composition.append(("batchnorm2d", name_id, [float32, B, C, H, W]))
 
             return f'{node.name} = nn.batchnorm2d[float32, {B}, {C}, {H}, {W}, "{name_id}"]({inp}, {gamma}, {beta}, {eps}, {running_mean}, {running_var})'
         else:
-            raise NotImplementedError(f"Unsupported shape for batchnorm2d: {input_shape}")
+            raise NotImplementedError(
+                f"Unsupported shape for batchnorm2d: {input_shape}"
+            )
