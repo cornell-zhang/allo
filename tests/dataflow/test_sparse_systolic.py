6387895420f9dec172ce0be3ff87a47db9b05df4
CynyuS <cynyu.shao@gmail.com>
2025-07-02 16:53:42 -0300

    Fixed functionality of systolic row wise product multiplier.

--- tests/dataflow/test_sparse_systolic.py
+++ tests/dataflow/test_sparse_systolic.py
@@ -9,61 +9,47 @@ import numpy as np
 import random
 
 M, N, K = 2, 2, 2
-# P0, P1 = M + 2, N + 2
-P0, P1 = 1, 1 # no fifos so you can straight up access all matrices
+P0, P1 = M + 2, N + 2
 
-# Create a sparse pattern mask for A (2:4 sparsity)
-# For each block of 4 elements, randomly select 2 positions to be zero
-MAXNZ = K//2
-Knz = int(MAXNZ)
-
+NZ = int(K // 2)
 
 @df.region()
 def top():
-    # Define FIFO streams for A values and their column-indices.
-    # Streams have depth (e.g. 4) and dimensions (Mt+2, Nt+2) to include boundaries.
     fifo_A = df.array(df.pipe(dtype=float32, shape=(), depth=4), shape=(P0, P1))
     fifo_idx = df.array(df.pipe(dtype=int32, shape=(), depth=4), shape=(P0, P1))
 
-    # Systolic kernel on one Mt-by-Nt tile of C.
     @df.kernel(mapping=[P0, P1])
-    def semm(A: float32[M, Knz], A_in: int32[M, Knz], B: float32[K, N], C: float32[M, N]):
+    def gemm(A: float32[M, NZ], A_in: int32[M, NZ], B: float32[K, N], C: float32[M, N]):
         i, j = df.get_pid()
-        # skip corners
+        # periperals kernels
         with allo.meta_if(i in {0, M + 1} and j in {0, N + 1}):
             pass
-
-        # left end we create fifo for A values and A nonzero column indices.
         with allo.meta_elif(j == 0):
-            # for each nonzero in row (i-1) of A_tile:
-            for k in range(Knz):
-                fifo_A[i, j+1].put(A[i-1, k])
-                fifo_idx[i, j+1].put(A_in[i-1, k])
-
+            # i > 0
+            for knz in range(NZ):
+                fifo_A[i, j + 1].put(A[i - 1, knz])
+                fifo_idx[i, j + 1].put(A_in[i - 1, knz])
+        with allo.meta_elif(i == 0):
+            # j > 0
+            pass
         # drain
-        with allo.meta_elif(i == 0 or i == M + 1 and j > 0):
-            # don't need to collect at bottom or load at top because 
-            # B direct access and is not draining at bottom
-            pass 
-
+        with allo.meta_elif(i == M + 1 and j > 0):
+            pass
         with allo.meta_elif(j == N + 1 and i > 0):
-            # drain matrix A
-            for k in range(K):
+            for k in range(NZ):
                 a: float32 = fifo_A[i, j].get()
-
-        # cherry pick values of B to multiply with A, forward A and
+                idx: int32 = fifo_idx[i, j].get()
+        # main body
         with allo.meta_else():
-            c_val: float32 = 0
-            for k in range(Knz):
-                a_val:  float32   = fifo_A[i, j].get()
-                idx:    int32  = fifo_idx[i, j].get()
-                b_val:  float32   = B[idx, j-1]
-                c_val += a_val * b_val
-                # Propagate A and index to the next PE to the right.
-                fifo_A[i, j+1].put(a_val)
-                fifo_idx[i, j+1].put(idx)
-            # put accumulated result in PE
-            C[i-1, j-1] = c_val
+            c: float32 = 0
+            for k in range(NZ):
+                a: float32 = fifo_A[i, j].get()
+                idx: int32 = fifo_idx[i, j].get()
+                b: float32 = B[idx, j - 1]
+                c += a * b
+                fifo_A[i, j + 1].put(a)
+                fifo_idx[i, j + 1].put(idx)
+            C[i - 1, j - 1] = c
 
 
 def create_sparse_matrix(m, k, sparsity_ratio):
@@ -122,14 +108,14 @@ def test_sparse_systolic():
     expected = np.dot(A_dense, B)
     print(expected)
 
-    np.testing.assert_allclose(C, np.dot(A, B), atol=1e-5)
+    np.testing.assert_allclose(C, np.dot(A_dense, B), atol=1e-5)
     print("Dataflow Simulator Passed!")
 
     mod = df.build(top)
     if hls.is_available("vitis_hls"):
         C = np.zeros((M, N), dtype=np.float32)
         mod(A, B, C)
-        np.testing.assert_allclose(C, np.dot(A, B), atol=1e-5)
+        np.testing.assert_allclose(C, np.dot(A_dense, B), atol=1e-5)
         print("Passed!")
 
 
