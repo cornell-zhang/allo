4544dc76ebe587705c2fed66060ac57a1cf9e226
chrisfxu <chrisfxu@gmail.com>
2025-07-15 17:52:34 -0400

    wip

--- tests/dataflow/aie/test_cannon.py
+++ tests/dataflow/aie/test_cannon.py
@@ -7,13 +7,13 @@ from allo.ir.types import int32
 from allo.memory import Layout
 import numpy as np
 
-LyA = Layout("S0R")
-LyB = Layout("RS1")
+LyA = Layout("S0S1")
+LyB = Layout("S0S1")
 LyC = Layout("S0S1")
 
 def _test_cannon():
     Ty = int32
-    M, K, N = 16, 16, 16
+    M, K, N = 8, 8, 8
     P = 2
     m, k, n = M // P, K // P, N // P
 
@@ -23,23 +23,28 @@ def _test_cannon():
         B_pipe = df.array(df.pipe(dtype=Ty, shape=(k, n), depth=2), shape=(P, P))
 
         @df.kernel(mapping=[P, P])
+        def init(A: Ty[M, K] @ LyA, B: Ty[K, N] @ LyB, C: Ty[M, N] @ LyC):
+            pi, pj = df.get_pid()
+
+            A_pipe[pi, (pj - pi) % P].put(A)
+            B_pipe[(pi - pj) % P, pj].put(B)
+
+        @df.kernel(mapping=[P, P])
         def cannon(A: Ty[M, K] @ LyA, B: Ty[K, N] @ LyB, C: Ty[M, N] @ LyC):
+            pi, pj = df.get_pid()
 
             C_local: Ty[m, n] = 0
 
-            with allo.meta_for(P) as l:
-                A_local: Ty[m, k] = 0
-                B_local: Ty[k, n] = 0
+            for _ in range(P):
+                A_out: Ty[m, k] = A_pipe[pi, pj].get()
+                B_out: Ty[k, n] = B_pipe[pi, pj].get()
+                C_local[:, :] += allo.matmul(A_out, B_out)
+                A_pipe[pi, (pj - 1) % P].put(A_out)
+                B_pipe[(pi - 1) % P, pj].put(B_out)
 
-                for i in range(m):
-                    for j in range(k):
-                        A_local[i, j] = A[i, l * k + j]
-
-                for i in range(k):
-                    for j in range(n):
-                        B_local[i, j] = B[l * k + i, j]
-
-                C_local[:, :] += allo.matmul(A_local, B_local)
+            # # Drain pipe
+            # A_pipe[0, 0].get()
+            # B_pipe[0, 0].get()
 
             C[:, :] = C_local
 
