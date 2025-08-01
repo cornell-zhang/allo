be88fbb75369160f9949be0bbd38cb65145e6f6c
Fangtangtang <2167295472@qq.com>
2025-08-01 16:10:45 -0400

    [test] multi-output testcase

--- tests/dataflow/aie/test_multi_output.py
+++ tests/dataflow/aie/test_multi_output.py
@@ -7,38 +7,33 @@ from allo.ir.types import int16
 from allo.memory import Layout
 import numpy as np
 
-# RRxRS->RS
-# RSxSR->RR
-LyW1 = Layout("RS0")
-LyW2 = Layout("S0R")
 
-
-def _test_gemm_1D():
+def _test_increase_decrease():
     LyA = Layout("S0R")
 
     Ty = int16
-    M, N, K = 16, 16, 16
-    P0 = 2
+    M, N = 64, 64
+    P0 = 4
 
     @df.region()
     def top():
         @df.kernel(mapping=[P0])
-        def gemm(A: Ty[M, K] @ LyA, B: Ty[K, N], C: Ty[M, N] @ LyA):
-            C[:, :] = allo.matmul(A, B)
+        def inc(A: Ty[M, N] @ LyA, C: Ty[M, N] @ LyA):
+            C[:, :] = allo.add(A, 1)
 
         @df.kernel(mapping=[P0])
-        def mmeg(D: Ty[M, K] @ LyA, E: Ty[K, N], F: Ty[M, N] @ LyA):
-            F[:, :] = allo.matmul(D, E)
+        def dec(D: Ty[M, N] @ LyA, F: Ty[M, N] @ LyA):
+            F[:, :] = allo.add(D, -1)
 
     mod = df.build(top, target="aie-mlir")
-    A = np.random.randint(0, 64, (M, K)).astype(np.int16)
-    B = np.random.randint(0, 64, (K, N)).astype(np.int16)
+    A = np.random.randint(0, 64, (M, N)).astype(np.int16)
     C = np.zeros((M, N)).astype(np.int16)
     F = np.zeros((M, N)).astype(np.int16)
-    mod(A, B, C, A, B, F)
-    np.testing.assert_allclose(C, A @ B, atol=1e-5)
+    mod(A, C, A, F)
+    np.testing.assert_allclose(C, A + 1, atol=1e-5)
+    np.testing.assert_allclose(F, A - 1, atol=1e-5)
     print("PASSED!")
 
 
 if __name__ == "__main__":
-    _test_gemm_1D()
+    _test_increase_decrease()
