894d7edaf68a5d01e45416c6973b96ffbd0c4a88
chhzh123 <hc676@cornell.edu>
2026-01-11 22:43:10 -0500

    Update alloc

--- tests/dataflow/test_hierachical.py
+++ tests/dataflow/test_hierachical.py
@@ -23,28 +23,41 @@ def inner(A: float32[M, K], B: float32[K, N], C: float32[M, N]):
 
 
 @df.region()
-def top(A: float32[M, K], B: float32[K, N], C: float32[M, N]):
-    @df.kernel(mapping=[2], args=[A, B, C])
-    def wrapper(local_A: float32[M, K], local_B: float32[K, N], local_C: float32[M, N]):
-        inner(local_A, local_B, local_C)
+def top(A: float32[M, K], B: float32[K, N], C1: float32[M, N], C2: float32[M, N]):
+    @df.kernel(mapping=[2], args=[A, B, C1, C2])
+    def wrapper(
+        local_A: float32[M, K],
+        local_B: float32[K, N],
+        local_C1: float32[M, N],
+        local_C2: float32[M, N],
+    ):
+        i = df.get_pid()
+        with allo.meta_if(i == 0):
+            inner(local_A, local_B, local_C1)
+        with allo.meta_if(i == 1):
+            inner(local_A, local_B, local_C2)
 
 
 def test_hierachical_function():
     A = np.random.rand(M, K).astype(np.float32)
     B = np.random.rand(K, N).astype(np.float32)
-    C = np.zeros((M, N), dtype=np.float32)
+    C1 = np.zeros((M, N), dtype=np.float32)
+    C2 = np.zeros((M, N), dtype=np.float32)
 
-    # sim_mod = df.build(top, target="simulator")
-    # sim_mod(A, B, C)
-    # np.testing.assert_allclose(C, np.dot(A, B), rtol=1e-5)
-    # print("Dataflow Simulator Passed!")
+    sim_mod = df.build(top, target="simulator")
+    sim_mod(A, B, C1, C2)
+    np.testing.assert_allclose(C1, np.dot(A, B), rtol=1e-5)
+    np.testing.assert_allclose(C2, np.dot(A, B), rtol=1e-5)
+    print("Dataflow Simulator Passed!")
 
     mod = df.build(top)
     print(mod.module)
     if hls.is_available("vitis_hls"):
-        C = np.zeros((M, N), dtype=np.float32)
-        mod(A, B, C)
-        np.testing.assert_allclose(C, np.dot(A, B), rtol=1e-5, atol=1e-5)
+        C1 = np.zeros((M, N), dtype=np.float32)
+        C2 = np.zeros((M, N), dtype=np.float32)
+        mod(A, B, C1, C2)
+        np.testing.assert_allclose(C1, np.dot(A, B), rtol=1e-5, atol=1e-5)
+        np.testing.assert_allclose(C2, np.dot(A, B), rtol=1e-5, atol=1e-5)
         print("Success!")
 
 
